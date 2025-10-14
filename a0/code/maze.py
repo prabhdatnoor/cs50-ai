854a77b10f46f878387e851ab3203a03e582eabf
Prabhnoor <23198519+prabhdatnoor@users.noreply.github.com>
2025-10-14 18:18:28 -0400

    frontiers

--- a0/code/maze.py
+++ a0/code/maze.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 
 @dataclass
 class Node:
-    state: list["Node"]
+    state: str
     parent: "Node | None"
     action: list[int]
 
@@ -13,5 +13,28 @@ class Node:
 class StackFrontier:
     frontier: list[Node]
 
+    def add(self, n: Node):
+        self.frontier.append(n)
 
-print(Node(state=[], parent=None, action=[]))
+    def empty(self):
+        return len(self.frontier) == 0
+
+    def exists(self, n: Node):
+        return any(node.state == n.state for node in self.frontier)
+
+    def peek(self):
+        return self.frontier[-1]
+
+    def pop(self):
+        if self.empty():
+            raise Exception("Frontier empty!")
+
+        return self.frontier.pop()
+
+
+class QueueFrontier(StackFrontier):
+    def remove(self):
+        if self.empty():
+            raise Exception("Frontier empty!")
+
+        return self.frontier.pop(0)
